04a22cbec50d7720891e823fb9b1661bdc31fe47
Ye-Ji Kim <ykim0113@gmail.com>
2018-01-28 17:02:07 -0500

    Updated AntLab

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
           toPrint += p4.toString("gburdell4");
           //Person5 replace P4 with your name
           //and gburdell5 with your gt id
-          Person5 p5 = new Person5("Ye-jiKim");
+          Person5 p5 = new Person5("Ye-ji Kim");
           toPrint += p5.toString("ykim722");
 		  
         System.out.println(toPrint);
