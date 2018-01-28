90a6893eeebd0de5ec8fc69afda0c8f17e3dc25e
Tony Wu <twu314@gatech.edu>
2018-01-28 17:17:21 -0500

    Revert "Update Ant"
    This reverts commit 938ac741146735d1e527e8dade23e548f01d9309, reversing
    changes made to 5e93c7ab51383953108af61cd59703864288f611.

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -71,8 +71,10 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell4 with your gt id
 		  Person4 p4 = new Person4("Younmin Bae");
 		  toPrint += p4.toString("ybae35");
-          Person5 p5 = new Person5("Ye-ji Kim");
-          toPrint += p5.toString("ykim722");
+		  //Person5 replace P4 with your name
+		  //and gburdell5 with your gt id
+		  Person5 p5 = new Person5("P5");
+		  toPrint += p5.toString("gburdell5");
 		  
         System.out.println(toPrint);
         
